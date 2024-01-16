558fc100dcb670f9a5e3bc3ab16bd9fb926c4e5f
DavideGila <davide.gila@gmail.com>
2024-01-16 16:11:31 +0100

    added select for categories

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -30,6 +30,20 @@
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
     </div>
+
+    <div class="mb-3">
+        <label for="category">Select Category</label>
+        <select class="form-control @error('category_id') is-invalid @enderror" name="category_id" id="category_id">
+            <option value="">Select a Category</option>
+            @foreach ($categories as $category)
+                <option value="{{$category->id}}" {{ old('category_id', $project->category_id) == $category->id ? 'selected' : '' }}>{{$category->name}}</option>
+
+            @endforeach
+        </select>
+        @error('category_id')
+            <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
+    </div>
     <button type="submit" class="btn btn-success">Save</button>
     <button type="reset" class="btn btn-primary">Reset</button>
 
