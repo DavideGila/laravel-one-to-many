55921181b4b3c8439deb98953e7f0f8816c19bd4
DavideGila <davide.gila@gmail.com>
2024-01-16 16:21:31 +0100

    added a select for categories

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -3,34 +3,51 @@
     <section class="container">
         <h1>Project Create</h1>
         <form action="{{ route('admin.projects.store') }}" enctype="multipart/form-data" method="POST">
-        @csrf
-     <div class="mb-3">
-            <label for="title">Title</label>
-            <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title"
-                required value="{{ old('title') }}">
-            @error('title')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-    </div>
+            @csrf
+            <div class="mb-3">
+                <label for="title">Title</label>
+                <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title"
+                    required value="{{ old('title') }}">
+                @error('title')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
 
-    <div class="mb-3">
-        <label for="body">Body</label>
-        <textarea class="form-control @error('body') is-invalid @enderror" name="body" id="body" cols="30" rows="10">
+            <div class="mb-3">
+                <label for="body">Body</label>
+                <textarea class="form-control @error('body') is-invalid @enderror" name="body" id="body" cols="30"
+                    rows="10">
         {{ old('body') }}
         </textarea>
-        @error('body')
-            <div class="invalid-feedback">{{ $message }}</div>
-        @enderror
-    </div>
-    <div class="mb-3">
+                @error('body')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="mb-3">
                 <label for="image">Image</label>
-                <input type="file" class="form-control @error('image') is-invalid @enderror" name="image" id="image" value="{{old('image')}}">
+                <input type="file" class="form-control @error('image') is-invalid @enderror" name="image"
+                    id="image" value="{{ old('image') }}">
                 @error('image')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
-    </div>
-    <button type="submit" class="btn btn-success">Save</button>
-    <button type="reset" class="btn btn-primary">Reset</button>
+            </div>
+
+            <div class="mb-3">
+                <label for="category">Select Category</label>
+                <select class="form-control @error('category_id') is-invalid @enderror" name="category_id" id="category_id">
+                    <option value="">Select a Category</option>
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}"
+                            {{ old('category_id', $project->category_id) == $category->id ? 'selected' : '' }}>
+                            {{ $category->name }}</option>
+                    @endforeach
+                </select>
+                @error('category_id')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+            <button type="submit" class="btn btn-success">Save</button>
+            <button type="reset" class="btn btn-primary">Reset</button>
 
         </form>
     </section>
